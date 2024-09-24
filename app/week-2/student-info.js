5f4fa2b53af9baaf685e13691f76f181fab2cb1f
studentsait <148035208+studentsait@users.noreply.github.com>
2024-09-24 15:38:01 -0600

    step
    step

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Kartikey Bhardwaj</p>
+      <p>
+        <Link href="https://github.com/studentsait/cprg306-assignments">
+          https://github.com/studentsait/cprg306-assignments
+        </Link>
+      </p>
+    </div>
+  );
+}
