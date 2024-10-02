53a011a4804602031971630f3936e6d376a60f24
studentsait <148035208+studentsait@users.noreply.github.com>
2024-10-01 23:26:53 -0600

    done week 3

--- app/page.js
+++ app/page.js
@@ -7,6 +7,9 @@ export default function Home() {
       <p>
         <Link href="/week-2">Week 2 Assignment</Link>
       </p>
+      <li>
+        <Link href="/week-3">Week 3</Link>
+      </li>
     </main>
   );
 }
