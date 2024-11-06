309c6f422390b8b3497c3a5ab38c97f25a555464
studentsait <148035208+studentsait@users.noreply.github.com>
2024-11-06 09:28:21 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -20,10 +20,10 @@ export default function Home() {
         <Link href="/week-6">Week 6</Link>
         </li>
         <li>
-        <Link href="/week-7">Week 6</Link>
+        <Link href="/week-7">Week 7</Link>
         </li>
         <li>
-        <Link href="/week-8">Week 6</Link>
+        <Link href="/week-8">Week 8</Link>
         </li>
 
     </main>
